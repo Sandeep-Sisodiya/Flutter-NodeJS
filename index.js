6bc4ef637f3ba04d69df0ba5f2d8642fbd592a57
Sandeep Sisodiya <sandeepsisodiya16092004@gmail.com>
2026-06-12 00:28:52 +0530

    adding basic structure

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.json({
+        name: "anyName",
+        age: "anyAge",
+        address: "anyAddress"
+    });
+});
+
+app.listen(1000, function () {
+    console.log("Server is running in port http://localhost:1000");
+})
